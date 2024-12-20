4ac72d3ffe8cb2fcd02c30d2d4639c82db4363ed
lirajoaop <joaopedroababa132@gmail.com>
2024-12-19 22:40:17 -0300

    add ternary on tailwind and stylize ver mais button

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -7,10 +7,13 @@ export interface Props {
 }
 
 export default function MovieCard(props: Props) {
+
     const movie = props.movie
+
   return (
+
     <li className="text-white w-full h-full rounded-lg overflow-hidden relative group cursor-pointer">
-        <div className="w-full h-full object-cover opacity-100 group-hover:opacity-50 transition-opacity duration-[500]">
+        <div className="w-full h-full object-cover opacity-100 group-hover:opacity-30 transition-opacity duration-[500] ">
             <img
                 src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
                 alt={movie.title} 
@@ -18,11 +21,22 @@ export default function MovieCard(props: Props) {
         </div>
 
 
-        <div className="absolute bottom-0 bg-[linear-gradient(#00000000,#000000_90%)] h-[200px] w-full flex flex-col justify-end p-[10px]">
-            <p className="text-[#f1f5f9] text-xs">
+        <div className={`absolute bottom-0 bg-[linear-gradient(#00000000,#000000_90%)] w-full flex flex-col justify-end p-[10px] transition-all duration-[600] ${
+      movie.overview
+        ? "group-hover:h-[100px] h-[50px]"
+        : "group-hover:h-[50px] h-[50px]"
+    }`}
+         >
+            <p className="text-[#f1f5f9] text-xs mb-1">
                 {movie.title}
             </p>
-            <StarRating rating={movie.vote_average}/>
+
+            {movie.vote_average > 0 &&
+            <StarRating
+                 rating={movie.vote_average}
+            />
+            }
+
             <div className="text-[#f1f5f9] text-xs mt-2 h-0 opacity-0 group-hover:opacity-100 group-hover:h-[100px] transition-all duration-[600]">
                 {movie.overview &&
                 <p className="text-[#fff000]">
@@ -32,7 +46,8 @@ export default function MovieCard(props: Props) {
                     }   
                 </p>
                 }
-                <button className="{btn-default}">
+
+                <button className="bg-[#6016ff] py-[6px] px-2 rounded-[4px] mt-[10px] text-white border-none cursor-pointer w-full">
                     Ver mais
                 </button>
             </div>
